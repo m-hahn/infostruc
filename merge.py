44d241673ada0e40d70e02e9534a5853755ebbb0
Michael Hahn <mhahn29@gmail.com>
2019-11-30 17:07:32 -0800

    code

--- merge.py
+++ merge.py
@@ -11,7 +11,6 @@ import sys
 import xml.etree.ElementTree as ET
 
 def recursivelyBuildTree(lm, SENT_ID):
- #  print(lm.attrib)
    result = {"id" : lm.attrib["id"], "children" : [], "tfa" : "NULL", "t_lemma" : "NULL"}
    for child in lm:
       if child.tag.endswith("children"):
@@ -21,7 +20,6 @@ def recursivelyBuildTree(lm, SENT_ID):
         result["tfa"] = child.text
       elif child.tag.endswith("t_lemma"):
         result["t_lemma"] = child.text
-#      print(child)
    wordsToData[SENT_ID][result["id"][2:]] = result
    return result
 
@@ -32,18 +30,10 @@ for directory in os.listdir("/u/scr/mhahn/CORPORA/czech_pdt/PDT3.5/data/tamw/"):
   BASE_DIR = "/u/scr/mhahn/CORPORA/czech_pdt/PDT3.5/data/tamw/"+directory
   files = sorted(list(set([x[:-5] for x in os.listdir(BASE_DIR)])))
   print(files)
-#  quit()
   for fileName in files:
     print(fileName) 
     tree_m = ET.fromstring(gzip.open(BASE_DIR+"/"+fileName+".m.gz", "r").read())
     tree_t = ET.fromstring(gzip.open(BASE_DIR+"/"+fileName+".t.gz", "r").read())
-    
-#    print(tree_t)
- #   print(tree_t.tag)
-  #  print(tree_t.attrib)
-    
-    
-    
     
     for child in tree_t:
        if child.tag.endswith("trees"):
@@ -51,16 +41,10 @@ for directory in os.listdir("/u/scr/mhahn/CORPORA/czech_pdt/PDT3.5/data/tamw/"):
             SENT_ID = sentence.attrib["id"][2:]
             wordsToData[SENT_ID] = {}
             for annotation in sentence:
-                #print(annotation)
                 if annotation.tag.endswith("children"):
                     for lm in annotation:
                       fullTree = recursivelyBuildTree(lm, SENT_ID) #print("LM", lm.attrib)
-                 #     print("TREE")
-                  #    print(fullTree)
-         #   break
-#         break
     
-   # print(wordsToData)
     
     # c contrastive & bound
     # f non-bound
@@ -72,30 +56,21 @@ for directory in os.listdir("/u/scr/mhahn/CORPORA/czech_pdt/PDT3.5/data/tamw/"):
            sentence = []
            wordsToData[SENT_ID]["linearized"] = sentence
            for child2 in child:
-               #print(child2.attrib)
                dataHere = wordsToData[SENT_ID].get(child2.attrib["id"][2:], {})
                wordsToData[SENT_ID][child2.attrib["id"][2:]] = dataHere
-               #print(dataHere)
                wordForm = None
                for anno in child2:
                    if anno.tag.endswith("form"):
                       dataHere["wordForm"] = anno.text
                       wordForm = anno.text
-                #      print("WORD FORM", wordForm)
                       break
                sentence.append((wordForm, dataHere))
-#           print(sentence)
            surfaceString = " ".join([x[0] for x in sentence])
-#           print(surfaceString)
- #          break
-#quit()
-
-#print(wordsToData)
 
 counter = 0
 for partition in ["test", "dev", "train"]:
   with open("/u/scr/mhahn/CORPORA/czech_pdt_infostruc/"+partition+".conllu", "w") as outFile:
-   with open("/u/scr/corpora/Universal_Dependencies/Universal_Dependencies_2.4/ud-treebanks-v2.4/UD_Czech-PDT/cs_pdt-ud-train.conllu", "r") as inFile:
+   with open("/u/scr/corpora/Universal_Dependencies/Universal_Dependencies_2.4/ud-treebanks-v2.4/UD_Czech-PDT/cs_pdt-ud-"+partition+".conllu", "r") as inFile:
       sentence = []
       attribs = {}
       for line in inFile:
@@ -105,14 +80,11 @@ for partition in ["test", "dev", "train"]:
            try:
               index = line.index("=")
               attribs[line[:index-1]] = line[index+2:]
-   #           print((line[:index-1], line[index+1:]))
            except ValueError:
             _ = 0
         elif len(line) <= 1:
           if len(attribs) > 0:
              counter += 1
-    #         if counter % 1000 == 0:
-   #               print(counter)
              sentenceID = attribs['# sent_id']
              if sentenceID in wordsToData:
                 annotated = wordsToData[sentenceID]["linearized"]
@@ -121,7 +93,6 @@ for partition in ["test", "dev", "train"]:
                 if "".join(fromAnnotation) != fromUD.replace(" ", ""):
                   print((fromAnnotation, fromUD), file=sys.stderr)
                 print(fromAnnotation)
-   #             print("\n".join(sentence))
                 lastWord = sentence[-1]
                 counterInAnnotation = 0
                 counterInUD = 0
@@ -134,22 +105,25 @@ for partition in ["test", "dev", "train"]:
                    if form != fromAnnotation[counterInAnnotation]:
                      print((form, fromAnnotation[counterInAnnotation]), file=sys.stderr)
                    sentence[counterInUD]+="\t"+annotated[counterInAnnotation][1].get("tfa", "N")
+                   print(line[0], "-" in line[0])
+
+                   # One token in PDT corresponding to a sequence in the UD version
                    if "-" in line[0]:
                       start, end = line[0].split("-")
-                      for i in range(counterInUD+1, 2 + int(end) - int(start)):
-                         sentence[counterInUD]+="\t"+annotated[counterInAnnotation][1].get("tfa", "N")
+                      for i in range(counterInUD+1, counterInUD + 2 + int(end) - int(start)):
+                         sentence[i]+="\t"+annotated[counterInAnnotation][1].get("tfa", "N")
                       counterInUD += 2 + int(end) - int(start)
                    else:
                       counterInUD += 1
                    counterInAnnotation += 1
-   #             assert len(fromAnnotation) == int(lastWord[:lastWord.index("\t")])
-   #             break
              else:
                 print(("MISSING SENTENCE", sentenceID), file=sys.stderr)
                 for i in range(len(sentence)):
                     sentence[i] += "\tUNK"
 
              for line in sentence:
+                assert line[-1] in ["N", "t", "f", "c"] or line.endswith("UNK") or line.endswith("NULL"), line
+                assert "\n" not in line
                 print(line, file=outFile)
 
           print("\n\n", file=outFile)
